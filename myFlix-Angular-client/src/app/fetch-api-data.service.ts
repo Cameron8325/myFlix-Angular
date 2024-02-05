5e3c7f4fb09a5769fe35857afa9b454315f974e9
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2024-02-05 17:24:26 -0500

    Initial Commit, added api fetching

---
+++ myFlix-Angular-client/src/app/fetch-api-data.service.ts
@@ -0,0 +1,110 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const apiUrl = 'https://camflixcf-73cf2f8e0ca3.herokuapp.com/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FetchApiDataService {
+  constructor(private http: HttpClient) {}
+
+  // User registration endpoint
+  public userRegistration(userDetails: any): Observable<any> {
+    return this.http.post(apiUrl + 'register', userDetails).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // User login endpoint
+  public userLogin(credentials: any): Observable<any> {
+    return this.http.post(apiUrl + 'login', credentials).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get all movies endpoint
+  public getAllMovies(): Observable<any> {
+    return this.http.get(apiUrl + 'movies').pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get one movie endpoint
+  public getMovie(title: string): Observable<any> {
+    return this.http.get(apiUrl + 'movies/' + title).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get director endpoint
+  public getDirector(name: string): Observable<any> {
+    return this.http.get(apiUrl + 'directors/' + name).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get genre endpoint
+  public getGenre(name: string): Observable<any> {
+    return this.http.get(apiUrl + 'genres/' + name).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get user endpoint
+  public getUser(username: string): Observable<any> {
+    return this.http.get(apiUrl + 'users/' + username).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get favorite movies for a user endpoint
+  public getFavoriteMovies(username: string): Observable<any> {
+    return this.http.get(apiUrl + 'users/' + username + '/movies').pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Add a movie to favorite movies endpoint
+  public addMovieToFavorites(username: string, movieId: string): Observable<any> {
+    return this.http.post(apiUrl + 'users/' + username + '/movies/' + movieId, {}).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Edit user endpoint
+  public editUser(username: string, userData: any): Observable<any> {
+    return this.http.put(apiUrl + 'users/' + username, userData).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Delete user endpoint
+  public deleteUser(username: string): Observable<any> {
+    return this.http.delete(apiUrl + 'users/' + username).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Delete a movie from favorite movies endpoint
+  public deleteMovieFromFavorites(username: string, movieId: string): Observable<any> {
+    return this.http.delete(apiUrl + 'users/' + username + '/movies/' + movieId).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
+}
