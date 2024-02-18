b4db1f8ebe23cc8540dc35ffb65929e35eb6d672
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2024-02-18 14:29:04 -0500

    Nesting issue resolved?

---
+++ .angular/cache/17.1.2/vite/deps_temp_a84e1f50/@angular_material_button.js
@@ -0,0 +1,36 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-W7Q6NXNG.js";
+import "./chunk-DMHO4GHN.js";
+import "./chunk-JZ7B6OX7.js";
+import "./chunk-7KTBYTKL.js";
+import "./chunk-SKS6UP27.js";
+import "./chunk-UKEHM6V6.js";
+import "./chunk-V2DXGMIT.js";
+import "./chunk-ZDOIMVJD.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
